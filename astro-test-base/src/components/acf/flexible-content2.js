06fd2fc94ec71ff84fcfcc8d859e4d727ecb55f0
rstringa <robertostringa@hotmail.com>
2023-12-13 16:25:43 -0300

    Components for content

---
+++ astro-test-base/src/components/acf/flexible-content2.js
@@ -0,0 +1,8 @@
+export function flexibleContent2(flexibleGroup) {
+    return `
+    <div class="flexible-2 box"> 
+        <h4>${flexibleGroup?.titulo}</h4>
+        <p>${flexibleGroup?.texto}</p>   
+    </div>
+    `;
+  }
