843fd6524a9babb23fcf3fc2427b1b053fd18066
rstringa <robertostringa@hotmail.com>
2024-05-14 18:48:01 -0300

    Astro - Login con Google

--- astro-test-base-365/astro.config.mjs
+++ astro-test-base-365/astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-import auth from 'auth-astro';
 
 export default defineConfig({
   prefetch: true,
-  integrations:[auth()],
   output: 'server',
-  adapter: vercel()
-
+  adapter: vercel(),
 });
 // import node from '@astrojs/node';
 
---
+++ astro-test-base-365/src/components/ResendTemplate.js
@@ -0,0 +1,7 @@
+export function resendTemplate(email,name) {
+    return `
+    <p>Enviado por:</p><br>
+    <p>Email: ${email}</p><br>
+    <p>Nombre: ${name}</p><br>
+    `;
+  }
